f08033f76ab630a201139564a61c45d0dc7cbdf1
Malekulashter Khandwawala <sadar89@gmail.com>
2024-07-01 17:30:25 +0000

    Migrate models to database

--- Electroedge/settings.py
+++ Electroedge/settings.py
@@ -75,17 +75,23 @@ WSGI_APPLICATION = 'Electroedge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'electroedge',
+#             'USER': 'root',
+#             'PASSWORD': '1300',
+#             'HOST': 'localhost',
+#             'PORT': '3306',
+#         },
+# }
+
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'electroedge',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 
 # Password validation
